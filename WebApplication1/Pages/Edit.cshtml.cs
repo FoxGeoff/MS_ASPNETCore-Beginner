d5a3a10435481357048e05e13a9fac71e874e77b
Geoff <fox.geoffrey@gmail.com>
2018-01-16 11:40:23 -0800

    Completed CRUD

--- WebApplication1/Pages/Edit.cshtml.cs
+++ WebApplication1/Pages/Edit.cshtml.cs
@@ -4,14 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Pages
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _context;
 
+        public EditModel(AppDbContext context)
+        {
+            _context = context;
         }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Customer = await _context.Customers
+                .Where(c => c.Id == id)
+                .SingleAsync();
+
+            if (Customer == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();  
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+
+                throw new Exception($"Customer {Customer.Id} not found!", e);
+            }
+
+            return RedirectToPage("./Index");
+        }
+
     }
 }
